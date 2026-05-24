7c303810f59ef9633c20a14c9053fcb0e0e2615c
James Smith <james@loopj.com>
2026-05-23 17:08:27 -0700

    Move models to own lib

---
+++ lib/models.dart
@@ -0,0 +1,8 @@
+export 'src/models/auth.dart';
+export 'src/models/enums.dart';
+export 'src/models/media_items/audio_format.dart';
+export 'src/models/media_items/metadata.dart';
+export 'src/models/player.dart';
+export 'src/models/player_queue.dart';
+export 'src/models/queue_item.dart';
+export 'src/models/stream_details.dart';
--- lib/music_assistant.dart
+++ lib/music_assistant.dart
@@ -1,15 +1,4 @@
-// Export our core interfaces
 export 'src/client.dart';
 export 'src/connection.dart' show ConnectionState;
 export 'src/events.dart';
 export 'src/exceptions.dart';
-
-// Export our models
-export 'src/models/auth.dart';
-export 'src/models/enums.dart';
-export 'src/models/media_items/audio_format.dart';
-export 'src/models/media_items/metadata.dart';
-export 'src/models/player.dart';
-export 'src/models/player_queue.dart';
-export 'src/models/queue_item.dart';
-export 'src/models/stream_details.dart';
